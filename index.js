1b8a02a236d58c7d148d77f050ec7dcd7ab329dc
joaovictorpsantos <joaovictorsantoscpv@gmail.com>
2022-06-25 11:48:10 -0300

    config: eslint

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 import fetch from 'node-fetch';
 
-const argv = yargs(hideBin(process.argv)).argv;
+const { argv } = yargs(hideBin(process.argv));
 
 const word = argv._[0];
 
@@ -11,34 +11,34 @@ const data = await response.json();
 
 const { meanings } = data[0];
 
-console.log(``)
+console.log('');
 console.log(`Word: ${data[0].word}`);
-console.log(``)
+console.log('');
 
 for (const meaning of meanings) {
-    console.log(`Part of speech: ${meaning.partOfSpeech}`);
+  console.log(`Part of speech: ${meaning.partOfSpeech}`);
+  console.log('');
+
+  const { definitions, synonyms, antonyms } = meaning;
+
+  const definitionsArray = [];
+
+  for (const definition of definitions) {
+    definitionsArray.push(definition.definition);
+  }
+
+  if (definitions) {
+    console.log(`Definitions: ${definitionsArray.join(' ')}`);
     console.log('');
+  }
 
-    const { definitions, synonyms, antonyms } = meaning;
+  if (synonyms.length > 0) {
+    console.log(`Synonyms: ${synonyms.join(', ')}`);
+    console.log('');
+  }
 
-    let definitionsArray = [];
-
-    for (const definition of definitions) {
-        definitionsArray.push(definition.definition);
-    }
-
-    if (definitions) {
-        console.log(`Definitions: ${definitionsArray.join(" ")}`)
-        console.log(``) 
-    }
-
-    if (synonyms.length > 0) {
-        console.log(`Synonyms: ${synonyms.join(", ")}`)
-        console.log(``)
-    }
-
-    if (antonyms.length > 0) {
-        console.log(`Antonyms: ${antonyms.join(", ")}`)
-        console.log(``)
-    }
+  if (antonyms.length > 0) {
+    console.log(`Antonyms: ${antonyms.join(', ')}`);
+    console.log('');
+  }
 }
